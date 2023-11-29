d45d11106c25d710c369fa20c3e9bed6f28bc0b8
techmefr <compigni.gaetan@gmail.com>
2023-11-29 14:12:47 +0100

    add post for movies

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.post("/api/movies", movieControllers.postMovie);
 
 const usersControllers = require("./controllers/usersControllers");
 
