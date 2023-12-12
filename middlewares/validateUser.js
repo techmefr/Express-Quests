19be06159864d78b65f51a147b8cf873ece3f865
techmefr <compigni.gaetan@gmail.com>
2023-12-12 13:58:52 +0100

    add middleware

---
+++ middlewares/validateUser.js
@@ -0,0 +1,23 @@
+const Joi = require("joi");
+
+const userSchema = Joi.object({
+  email: Joi.string().email().max(255).required(),
+  firstname: Joi.string().max(255).required(),
+  lastname: Joi.string().max(255).required(),
+});
+
+const validateUser = (req, res, next) => {
+  const { firstname, lastname, email } = req.body;
+
+  const { error } = userSchema.validate(
+    { firstname, lastname, email },
+    { abortEarly: false }
+  );
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
+module.exports = validateUser;
